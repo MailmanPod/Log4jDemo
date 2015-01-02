8d8966fd586ceefebcaac48ebfaf71041dd7e2bd
Damian Bruera <brueradamian@gmail.com>
2015-01-02 18:03:35 -0300

    Proyect Maven - Log4jDemo - initial commit

---
+++ Log4jDemo/src/main/java/org/comcast/log4jDemo/Log4jDemo.java
@@ -0,0 +1,27 @@
+package org.comcast.log4jDemo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class Log4jDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss zZ");
+		String formatDate = sdf.format(new Date());
+		
+		System.out.println("Application started at: " + formatDate);
+		
+		Logger logger = LogManager.getLogger();
+		
+		logger.info("Info Message");
+		logger.debug("Debug msg");
+		logger.error("Corta la bocha");
+		logger.info("Class: " + Log4jDemo.class.getCanonicalName());
+	}
+}
